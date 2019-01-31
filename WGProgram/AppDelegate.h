4e994215beca1793a725dd4dde9f58fb36c4bbae
王刚 <wanggang@317hu.com>
2019-01-31 13:36:49 +0800

    Initial Commit

---
+++ WGProgram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WGProgram
+//
+//  Created by wanggang on 2019/1/31.
+//  Copyright © 2019 bozhong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WGProgram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WGProgram
+//
+//  Created by wanggang on 2019/1/31.
+//  Copyright © 2019 bozhong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
